dd7087db497a8f200a8657d849ecd822a43d66a4
Ilya55tr <tretyak.il.27@gmail.com>
2025-10-11 00:05:40 +0600

    написал шаблоны create allgenres и genre для GenreController

--- src/main/java/com/IlyaTr/movie_catalog/controllers/GenreController.java
+++ src/main/java/com/IlyaTr/movie_catalog/controllers/GenreController.java
@@ -1,6 +1,7 @@
 package com.IlyaTr.movie_catalog.controllers;
 
 import com.IlyaTr.movie_catalog.dto.GenreCreateEditDto;
+import com.IlyaTr.movie_catalog.dto.GenreReadDto;
 import com.IlyaTr.movie_catalog.services.GenreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,23 +9,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/genre")
+@RequestMapping("/genres")
 @RequiredArgsConstructor
 public class GenreController {
 
     private final GenreService genreService;
 
     @GetMapping("/create")
-    public String createGenre(){
+    public String createGenre(Model model){
+        model.addAttribute("genre", new GenreCreateEditDto());
         return "genre/create";
     }
 
     @PostMapping
-    public String createGenre(@ModelAttribute GenreCreateEditDto genreDto){
-        genreService.createGenre(genreDto);
-        return "redirect:/genre";
+    public String createGenre(@ModelAttribute GenreCreateEditDto genreDto, RedirectAttributes redirectAttributes){
+       GenreReadDto genreReadDto=genreService.createGenre(genreDto);
+//        if (true){
+//            redirectAttributes.addFlashAttribute("genre", genreDto);
+//            return "redirect:/genres/create";
+//        }
+        return "redirect:/genres/"+ genreReadDto.getId();
     }
 
     @GetMapping("/{id}")
@@ -42,7 +49,7 @@ public class GenreController {
     @PostMapping("/{id}/update")
     public String updateGenre(@PathVariable Integer id, @ModelAttribute GenreCreateEditDto genreDto){
         genreService.updateGenre(genreDto, id);
-        return "redirect:/genre/"+id;
+        return "redirect:/genres/"+id;
     }
 
     @PostMapping("/{id}/delete")
@@ -50,6 +57,6 @@ public class GenreController {
         if (!genreService.deleteGenre(id)){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        return "redirect:/genre";
+        return "redirect:/genres";
     }
 }
